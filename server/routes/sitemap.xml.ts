403ddcf3f51ad5d22743f815b44a1c3707255c0d
haodai <a596116@gmail.com>
2023-05-25 16:46:48 +0800

    sitemap

--- server/routes/sitemap.xml.ts
+++ server/routes/sitemap.xml.ts
@@ -1,24 +1,12 @@
 // import { serverQueryContent } from '#content/server'
 import { SitemapStream, streamToPromise } from 'sitemap'
-import axios from 'axios'
 export default defineEventHandler(async (event) => {
     // const docs = await serverQueryContent(event).find()
+    event.res.setHeader('Content-Type', 'application/xml')
     const sitemap = new SitemapStream({
-        hostname: 'https://wanghaodai.com'
+        hostname: 'https://wanghaodai.com',
+
     })
-    // const dad = await axios({
-    //     method: 'get',
-    //     url: 'https://870c-220-141-228-159.ngrok-free.app/user/role',
-    //     data: {
-    //         page: 1,
-    //         take: 10
-    //     },
-    //     headers: {
-    //         haodai: event.req.headers.host
-    //     }
-    // }).then((res) => {
-    //     // console.log(res.data)
-    // })
     const page = ['/', 'about', 'collection', 'posts', 'music']
     // for (const doc of docs) {
     page.forEach((doc) => {
