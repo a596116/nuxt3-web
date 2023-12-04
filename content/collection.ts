d1a6880a2d24134a47b52798b1771b9462da6fb3
haodai <haodai.wang@sshop.com.tw>
2023-12-04 15:09:08 +0800

    collection

--- content/collection.ts
+++ content/collection.ts
@@ -1,47 +1,47 @@
 export const collection = [
-    {
-        title: 'Web',
-        items: [
-            {
-                sitename: '後台管理系統',
-                url: 'https://admin.wanghaodai.com/login',
-                img: 'https://s2.loli.net/2023/02/08/X8yiSYwTF1CENme.png',
-                desc: '使用Vue3、echarts、Element plus、axios、pinia、firebase串接後台API開發的後台系統',
-            },
-            {
-                sitename: '個人博客',
-                url: 'https://haodai-blog.web.app/',
-                img: 'https://s2.loli.net/2023/02/08/KjLxycGCJoOM43s.png',
-                desc: 'VuePress搭建的博客網站，存放學習資料',
-            },
-            {
-                sitename: '畢業專題介紹',
-                url: 'https://puds-12564.web.app/home',
-                img: 'https://s2.loli.net/2023/02/08/M6BH5PkFOsbC2TQ.png',
-                desc: '靜宜大學-資料科學暨大數據分析與應用學系-深度學習在太陽光電模組的熱影像缺陷自動識別系統的介紹網頁',
-            },
-        ],
-    },
-    {
-        title: 'API',
-        items: [
-            {
-                sitename: 'Nodejs後端',
-                url: 'https://github.com/a596116/nestjs',
-                img: 'https://s2.loli.net/2023/02/08/qgFMnxeBVuEH13G.png',
-                desc: '使用Nestjs搭配Mongodb資料庫開發RESTful API',
-            },
-        ],
-    },
-    {
-        title: 'LineBot',
-        items: [
-            {
-                sitename: '寵愛',
-                url: 'https://github.com/a596116/petLove',
-                img: 'https://s2.loli.net/2023/02/08/hBcqA3fopYHQnxj.png',
-                desc: '使用python + Django + Web 開發的LineBot',
-            },
-        ],
-    },
+  {
+    title: 'Web',
+    items: [
+      {
+        sitename: '後台管理系統',
+        url: 'https://github.com/a596116/Admin',
+        img: 'https://s2.loli.net/2023/02/08/X8yiSYwTF1CENme.png',
+        desc: '使用Vue3、echarts、Element plus、axios、pinia、firebase串接後台API開發的後台系統',
+      },
+      {
+        sitename: '個人博客',
+        url: 'https://haodai-blog.web.app/',
+        img: 'https://s2.loli.net/2023/02/08/KjLxycGCJoOM43s.png',
+        desc: 'VuePress搭建的博客網站，存放學習資料',
+      },
+      {
+        sitename: '畢業專題介紹',
+        url: 'https://puds-12564.web.app/home',
+        img: 'https://s2.loli.net/2023/02/08/M6BH5PkFOsbC2TQ.png',
+        desc: '靜宜大學-資料科學暨大數據分析與應用學系-深度學習在太陽光電模組的熱影像缺陷自動識別系統的介紹網頁',
+      },
+    ],
+  },
+  {
+    title: 'API',
+    items: [
+      {
+        sitename: 'Nodejs後端',
+        url: 'https://github.com/a596116/nestjs',
+        img: 'https://s2.loli.net/2023/02/08/qgFMnxeBVuEH13G.png',
+        desc: '使用Nestjs搭配Mongodb資料庫開發RESTful API',
+      },
+    ],
+  },
+  {
+    title: 'LineBot',
+    items: [
+      {
+        sitename: '寵愛',
+        url: 'https://github.com/a596116/petLove',
+        img: 'https://s2.loli.net/2023/02/08/hBcqA3fopYHQnxj.png',
+        desc: '使用python + Django + Web 開發的LineBot',
+      },
+    ],
+  },
 ]
